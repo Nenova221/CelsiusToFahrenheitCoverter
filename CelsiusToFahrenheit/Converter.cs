641f63d8eec42f07863682680b6d5f6454262c8e
Kalina <kalina.nenova@paisii.info>
2024-08-06 18:08:09 +0300

    First commit

---
+++ CelsiusToFahrenheit/Converter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CelsiusToFahrenheit
+{
+    public class Converter
+    {
+        public static float CelsiusToFahrenheit(float C)
+        {
+            float F = ((C * 9) / 5) + 32;
+
+            return F;
+        }
+    }
+}
